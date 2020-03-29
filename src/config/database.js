d384bc7ddd1f7d0821c5e952f5d04581021548d1
alemaocastro1986@github.com <alemaocastro1986@gmail.com>
2020-03-29 12:50:57 -0300

    added .env in .gitignore and change database config file

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const { resolve } = require('path');
 
 module.exports = {
   client: 'mssql',
   connection: {
-    host: 'localhost',
-    database: 'projects',
-    user: 'developer',
-    password: 'Developer@123',
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     options: {
       encrypt: true,
       enableArithAbort: true,
